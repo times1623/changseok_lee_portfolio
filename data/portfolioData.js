7f00936d85ab5529c44e489585c461d85b5a48af
Lee Changseok <leechangseok@Lees-MacBook-Pro.local>
2018-10-26 07:38:35 -0400

    boxframe design added

---
+++ data/portfolioData.js
@@ -0,0 +1,5 @@
+let portfolioData = {
+    arrName : ["Jeongyun Cho","Jorge Andrino"],
+    arrText : ["Kim Hanbok Project","First Year Integrated Project","Sci-Fi Fan","The Dark Mine","Small World"],
+    imgArr : ["cho1_l","cho2_l","andrino1_l","andrino2_l","andrino3_l"]
+};
